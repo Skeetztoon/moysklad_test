0c678161bab7a15ef164c6d9b00751f4e4434bbc
Skeetztoon <1014day@gmail.com>
2025-01-20 14:32:06 +0300

    Initial commit

---
+++ src/main/java/com/yurlov/moysklad_test/MoyskladTestApplication.java
@@ -0,0 +1,12 @@
+package com.yurlov.moysklad_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyskladTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoyskladTestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/yurlov/moysklad_test/MoyskladTestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.yurlov.moysklad_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyskladTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
